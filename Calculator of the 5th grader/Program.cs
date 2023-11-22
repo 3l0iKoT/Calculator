5fbde50a3e252338827eea3551a8559d58dea9f2
3l0i_Kot <arsenghj@mail.ru>
2023-11-22 07:53:10 +0500

    init commit

---
+++ Calculator of the 5th grader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_of_the_5th_grader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
