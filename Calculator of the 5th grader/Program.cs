b2d8355621d04f04be019ec29f441e6aeb8220d6
3l0i_Kot <arsenghj@mail.ru>
2023-11-22 09:26:33 +0500

    Changes in Program.cs
    Add selection menu

--- Calculator of the 5th grader/Program.cs
+++ Calculator of the 5th grader/Program.cs
@@ -10,6 +10,86 @@ namespace Calculator_of_the_5th_grader
     {
         static void Main(string[] args)
         {
+            int menuItem = 1;
+            while (true)
+            {
+                Menu(menuItem);
+                var key = Console.ReadKey();
+                if (key.Key.ToString() != "UpArrow" && key.Key.ToString() != "DownArrow" && key.Key.ToString() != "Enter")
+                {
+                    continue;
+                }
+                if (key.Key.ToString() == "UpArrow")
+                    menuItem = menuItem == 1 ? 6 : menuItem - 1;
+                else if (key.Key.ToString() == "DownArrow")
+                    menuItem = menuItem == 6 ? 1 : menuItem + 1;
+                else if (key.Key.ToString() == "Enter")
+                {
+                    if (menuItem == 6)
+                        break;
+                }
+            }
+        }
+
+        static void Menu(int menuItem)
+        {
+            Console.Clear();
+            Console.WriteLine("╔═══════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                                                       ║");
+            switch(menuItem)
+            {
+                case 1:
+                    Console.WriteLine("║ >>> Перевод целых чисел из любой системы счисления в любую другую <<< ║");
+                    Console.WriteLine("║             Перевод в римскую систему счисления и обратно             ║");
+                    Console.WriteLine("║             Процесс сложения в разных системах счисления              ║");
+                    Console.WriteLine("║             Процесс вычитания в разных системах счисления             ║");
+                    Console.WriteLine("║             Процесс умножения в разных системах счисления             ║");
+                    Console.WriteLine("║                          Выход из программы                           ║");
+                    break;
+                case 2:
+                    Console.WriteLine("║     Перевод целых чисел из любой системы счисления в любую другую     ║");
+                    Console.WriteLine("║         >>> Перевод в римскую систему счисления и обратно <<<         ║");
+                    Console.WriteLine("║             Процесс сложения в разных системах счисления              ║");
+                    Console.WriteLine("║             Процесс вычитания в разных системах счисления             ║");
+                    Console.WriteLine("║             Процесс умножения в разных системах счисления             ║");
+                    Console.WriteLine("║                          Выход из программы                           ║");
+                    break;
+                case 3:
+                    Console.WriteLine("║     Перевод целых чисел из любой системы счисления в любую другую     ║");
+                    Console.WriteLine("║             Перевод в римскую систему счисления и обратно             ║");
+                    Console.WriteLine("║         >>> Процесс сложения в разных системах счисления <<<          ║");
+                    Console.WriteLine("║             Процесс вычитания в разных системах счисления             ║");
+                    Console.WriteLine("║             Процесс умножения в разных системах счисления             ║");
+                    Console.WriteLine("║                          Выход из программы                           ║");
+                    break;
+                case 4:
+                    Console.WriteLine("║     Перевод целых чисел из любой системы счисления в любую другую     ║");
+                    Console.WriteLine("║             Перевод в римскую систему счисления и обратно             ║");
+                    Console.WriteLine("║             Процесс сложения в разных системах счисления              ║");
+                    Console.WriteLine("║         >>> Процесс вычитания в разных системах счисления <<<         ║");
+                    Console.WriteLine("║             Процесс умножения в разных системах счисления             ║");
+                    Console.WriteLine("║                          Выход из программы                           ║");
+                    break;
+                case 5:
+                    Console.WriteLine("║     Перевод целых чисел из любой системы счисления в любую другую     ║");
+                    Console.WriteLine("║             Перевод в римскую систему счисления и обратно             ║");
+                    Console.WriteLine("║             Процесс сложения в разных системах счисления              ║");
+                    Console.WriteLine("║             Процесс вычитания в разных системах счисления             ║");
+                    Console.WriteLine("║         >>> Процесс умножения в разных системах счисления <<<         ║");
+                    Console.WriteLine("║                          Выход из программы                           ║");
+                    break;
+
+                case 6:
+                    Console.WriteLine("║     Перевод целых чисел из любой системы счисления в любую другую     ║");
+                    Console.WriteLine("║             Перевод в римскую систему счисления и обратно             ║");
+                    Console.WriteLine("║             Процесс сложения в разных системах счисления              ║");
+                    Console.WriteLine("║             Процесс вычитания в разных системах счисления             ║");
+                    Console.WriteLine("║             Процесс умножения в разных системах счисления             ║");
+                    Console.WriteLine("║                      >>> Выход из программы <<<                       ║");
+                    break;
+            }
+            Console.WriteLine("║                                                                       ║");
+            Console.WriteLine("╚═══════════════════════════════════════════════════════════════════════╝");
         }
     }
 }
